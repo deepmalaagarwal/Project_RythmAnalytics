097f1074907b18bcde424238c6cfa06ed9fbdbef
Agarwal <dagarwal2@levi.com>
2020-01-09 02:38:00 -0800

    Recitfy app js file

--- RythmAnalytics/DataSets/Data_Cleanser.py
+++ RythmAnalytics/DataSets/Data_Cleanser.py
@@ -29,3 +29,43 @@ billboard_lyrics_data = pd.read_csv(file_to_load2, encoding = "ISO-8859-1")
 billboard_lyrics_data.head(500)
 billboard_lyrics_data.to_csv('cleaned_billboard_lyrics.csv', index=False)
 
+import os 
+import pandas as pd 
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Database Setup
+#################################################
+from flask_sqlalchemy import SQLAlchemy
+rds_connection_string = "RythmAnalytics/RythmAnalytics.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///RythmAnalytics/RythmAnalytics.sqlite"
+engine = create_engine(f'sqlite:///{rds_connection_string}')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+db = SQLAlchemy(app)
+
+#albums 
+# File to Load (Remember to Change These)
+file_to_load = "RythmAnalytics/DataSets/clean_albums.csv"
+billboard_lyrics_data = pd.read_csv("RythmAnalytics/DataSets/cleaned_billboard_lyrics.csv")
+# # Read Purchasing File and store into Pandas data frame
+albums_data = pd.read_csv(file_to_load)
+
+# # Create table for Sales analysis of albums
+albums_data.to_sql(name="db_albums", con=engine,if_exists = 'replace',index=False)
+# # Create table for Lyrics analysis of albums
+billboard_lyrics_data.to_sql(name="db_lyrics", con=engine,if_exists = 'replace',index=False)
+# # pd.read_sql_query('SELECT * FROM "db_lyrics"', con=engine)
+
